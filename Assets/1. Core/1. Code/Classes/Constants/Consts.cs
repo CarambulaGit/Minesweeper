8e5ae873d9118a78b1763a3e48c39e4706219425
Carambula <carambuletka@gmail.com>
2022-07-03 13:39:33 +0300

    add constants

---
+++ Assets/1. Core/1. Code/Classes/Constants/Consts.cs
@@ -0,0 +1,15 @@
+﻿public static class Consts {
+    #region Scene Names
+
+    public const string Boot = "Boot";
+    public const string MainMenu = "MainMenu";
+    public const string Game = "Game";
+
+    #endregion
+
+    #region PlayerPrefsKeys
+
+    public const string GameModeKey = "GameMode";
+
+    #endregion
+}
--- Assets/1. Core/1. Code/Classes/Consts.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class Consts {
-    public const string Boot = "Boot";
-    public const string MainMenu = "Main";
-    public const string Game = "Game";
-}
